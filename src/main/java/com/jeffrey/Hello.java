0c9a197007469e0a80b2a6a85bbdcea5688a4f63
Jeffrey <chiojhe@gmail.com>
2019-10-03 21:49:35 +0800

    Initial commit

---
+++ src/main/java/com/jeffrey/Hello.java
@@ -0,0 +1,10 @@
+package com.jeffrey;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("HelloWorld");
+        Person p = new Person();
+        p.hello();
+
+    }
+}
---
+++ src/main/kotlin/com/jeffrey/Hello.kt
@@ -0,0 +1,14 @@
+package com.jeffrey
+
+fun main() {
+//    println("HelloWorld")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello() {
+        println("Hello Kotlin")
+    }
+}
